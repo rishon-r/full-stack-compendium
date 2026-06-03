9d34d42cb71184a484058bcca5357526d3ae93e7
Rishon <rishonrajesh13@gmail.com>
2026-06-03 08:48:32 -0700

    notes on asyncio expanded

--- some-important-python-modules/concurrency-libraries/async_io_3.py
+++ some-important-python-modules/concurrency-libraries/async_io_3.py
@@ -18,7 +18,11 @@ async def main():
 
   async with asyncio.TaskGroup() as tg:
     # async with works exactly like with, but the setup and cleanup can be awaited
-    # You can use a regular with inside a coroutine, but you cannot await the context manager itself unless it's an async one
+    # This means that if setup and cleanup take time, your program will not idle
+    # instead, other tasks will be executed
+    # The same can be said about async for
+    # If the next iterable in the sequence cannot be immediately accessed and requires waiting
+    # async for allows other tasks to be executed while it waits
 
     # enumerate() wraps any iterable and yields (index, value) pairs as you loop. Instead of manually tracking a counter, you get both the position and the item together
     for delay, id in enumerate([2, 1, 3], start=1):
